64843676ea3c902dbc7fb98c26ecbf4b4bfd0447
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2020-06-09 14:43:14 +0300

    Update Program.cs

--- C# Basics/02. Simple Operations and Calculations/06. Circle Area and Perimeter/Program.cs
+++ C# Basics/02. Simple Operations and Calculations/06. Circle Area and Perimeter/Program.cs
@@ -12,15 +12,12 @@ namespace _06.Circle_Area_and_Perimeter
         {
 
             double r = double.Parse(Console.ReadLine());
-            // vuvejdame na konzolata chislo, koeto shte e radius na krug
+            
             double area = Math.PI * r * r;
-            // namirame liceto na kruga, chrez formulata
             double perimeter = 2 * Math.PI * r;
-            // namirame perimetura na kruga, chrez formulata
+            
             Console.WriteLine($"{area:F2}");
-            // vadim na konzolata liceto
             Console.WriteLine($"{perimeter:F2}");
-            // vadim na konzolata perimetura
         }
     }
 }
