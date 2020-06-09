266fd4ca4f8144d301bd50949044d7d0a5672463
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2020-06-09 14:29:47 +0300

    Create Program.cs

---
+++ C# Basics/02. Simple Operations and Calculations/03. Square Area/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Square_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = int.Parse(Console.ReadLine());
+            // vuvejdame na konzolata strana na kvadrat
+            int area = a * a;
+            // namirame liceto na kvadrata kato umnojim ednata strana po drugata
+            Console.WriteLine(area);
+            // vadim na konzolata liceto na kvadrata
+        }
+    }
+}
