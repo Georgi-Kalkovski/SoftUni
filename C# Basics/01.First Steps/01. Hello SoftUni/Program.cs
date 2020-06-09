a59112b76efc7937214dbf775ac7fdc27f98d7e3
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2020-06-09 14:23:41 +0300

    Update Program.cs

--- C# Basics/01.First Steps/01. Hello SoftUni/Program.cs
+++ C# Basics/01.First Steps/01. Hello SoftUni/Program.cs
@@ -1,18 +1,14 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace Program
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-
-            Console.WriteLine((3522 + 52353) * 23 - (2336 * 501 + 23432 - 6743) * 3);
-
-        }
-    }
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+		
+		Console.WriteLine("Hello SoftUni");
+			
+		}
+	}
 }
