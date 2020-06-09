bbe8317c8fb7b07fdcf4e1be85c474a103ff276f
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2020-06-09 14:26:15 +0300

    Create Program.cs

---
+++ C# Basics/01.First Steps/04. Rectangle Area/Program.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Program
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+			int sideA = int.Parse(Console.ReadLine());
+			int sideB = int.Parse(Console.ReadLine());
+			int rectangle = sideA * sideB;
+			Console.WriteLine(rectangle);
+			
+		}
+	}
+}
