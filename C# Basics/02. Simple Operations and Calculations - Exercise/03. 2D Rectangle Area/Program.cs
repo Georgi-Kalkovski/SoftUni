c3c30c1c8f63c61d39d665e969c7e8c7fca879fe
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2020-06-09 14:38:55 +0300

    Update Program.cs

--- C# Basics/02. Simple Operations and Calculations - Exercise/03. 2D Rectangle Area/Program.cs
+++ C# Basics/02. Simple Operations and Calculations - Exercise/03. 2D Rectangle Area/Program.cs
@@ -15,22 +15,15 @@ namespace _03._2D_Rectangle_Area
             double y1 = double.Parse(Console.ReadLine());
             double x2 = double.Parse(Console.ReadLine());
             double y2 = double.Parse(Console.ReadLine());
-            // vuvejdame koordinatite na pravougulnika
 
             double xy1 = Math.Abs(x1 - x2);
-            // namirame purvata strana (polzvame Math.Abs, za da napravim chisloto polojitelno)
             double xy2 = Math.Abs(y1 - y2);
-            // namirame vtorata strana (polzvame Math.Abs, za da napravim chisloto polojitelno)
 
             double area = xy1 * xy2;
-            // namirame liceto na pravougulnika
             double perimeter = 2 * (xy1 + xy2);
-            // namirame perimetura na pravougulnika
 
             Console.WriteLine($"{area:F2}");
-            // printirame liceto do vtoro chislo sled desetichnata zaptaq
             Console.WriteLine($"{perimeter:F2}");
-            // printirame perimetura do vtoro chislo sled desetichnata zaptaq
         }
     }
 }
