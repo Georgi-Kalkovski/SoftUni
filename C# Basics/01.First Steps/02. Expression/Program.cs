155d6d9d2d8ac2c90adef038163d5e2d395f7975
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2020-06-09 14:24:37 +0300

    Create Program.cs

---
+++ C# Basics/01.First Steps/02. Expression/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Program
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+		
+		Console.WriteLine((3522 + 52353) * 23 - (2336 * 501 + 23432 - 6743) * 3);
+			
+		}
+	}
+}
