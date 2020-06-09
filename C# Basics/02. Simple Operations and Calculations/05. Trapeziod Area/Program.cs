4252d69df290371c95bb2cb24ea732b459361d05
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2020-06-09 14:42:35 +0300

    Update Program.cs

--- C# Basics/02. Simple Operations and Calculations/05. Trapeziod Area/Program.cs
+++ C# Basics/02. Simple Operations and Calculations/05. Trapeziod Area/Program.cs
@@ -12,17 +12,12 @@ namespace _05.Trapeziod_Area
         {
 
             double b1 = double.Parse(Console.ReadLine());
-            // vuvejdame goleminata na ednata strana 
             double b2 = double.Parse(Console.ReadLine());
-            // vuvejdame goleminata na vtorata strana
             double h = double.Parse(Console.ReadLine());
-            // vuvejdame goleminata na dvete ravni strani na trapeca
 
             double area = ((b1 + b2) * h) / 2;
-            // namirame liceto na trapeca
 
             Console.WriteLine($"{area:F2}");
-            // vadim na konzolata liceto na trapeca 
         }
     }
 }
