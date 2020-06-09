53bd18149668086f79f9203f92f4635f5c09a3bb
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2020-06-09 14:30:41 +0300

    Create Program.cs

---
+++ C# Basics/02. Simple Operations and Calculations/04. Inches to Centimeters/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Inches_to_Centimeters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double inch = double.Parse(Console.ReadLine());
+            // vuvejdame cifra ot tip double na konzolata
+            double cm = inch * 2.54;
+            // prevrushtame inchovete v santimetri kato umnojim po 2.54
+            Console.WriteLine($"{cm:F2}");
+            // vadim na konzolata santimetrite do vtoriq znak sled cifrata
+        }
+    }
+}
