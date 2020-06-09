6c5e9dec4c61492261d1d1b2c5bb18885eeac7b0
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2020-06-09 14:28:05 +0300

    Create Program.cs

---
+++ C# Basics/02. Simple Operations and Calculations/01. Greeting by Name/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Greeting_by_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = Console.ReadLine();
+            // vuvejdame string ot konzolata
+            Console.WriteLine($"Hello, {name}!");
+            // izvajdame text sus stringa, koito sme vuveli
+
+        }
+    }
+}
