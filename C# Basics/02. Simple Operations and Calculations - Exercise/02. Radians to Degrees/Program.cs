9d787a0a6a245432b69687d032c4e9b7383e384d
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2020-06-09 14:39:49 +0300

    Update Program.cs

--- C# Basics/02. Simple Operations and Calculations - Exercise/02. Radians to Degrees/Program.cs
+++ C# Basics/02. Simple Operations and Calculations - Exercise/02. Radians to Degrees/Program.cs
@@ -12,11 +12,8 @@ namespace _02.Radians_to_Degrees
         {
 
             double rad = double.Parse(Console.ReadLine());
-            // vuvejdame na konzolata radius
             double deg = rad * 180 / Math.PI;
-            // izchislqvame ugula, chrez formulata radiusa umnojen na 180 deleno na PI
             Console.WriteLine(Math.Round(deg));
-            // printirame zakrugleniq ugul
         }
     }
 }
