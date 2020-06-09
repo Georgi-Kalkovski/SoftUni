95de07c4eea8fc54928031d2009d2e0207b6daa4
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2020-06-09 14:39:26 +0300

    Update Problem.cs

--- C# Basics/02. Simple Operations and Calculations - Exercise/01. USD to BGN/Problem.cs
+++ C# Basics/02. Simple Operations and Calculations - Exercise/01. USD to BGN/Problem.cs
@@ -12,11 +12,8 @@ namespace _01.USD_to_BGN
         {
 
             double usd = double.Parse(Console.ReadLine());
-            // vuvejdame na konzolata dolari
             double bgn = usd * 1.79549;
-            // prevrushtame dolarite v levove
             Console.WriteLine($"{bgn:F2}");
-            // printrame levovete na konzolata do vtoriq znak
         }
     }
 }
