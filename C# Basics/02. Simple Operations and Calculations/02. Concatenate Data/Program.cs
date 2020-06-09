630f3bc8a7b8a7950ea10ed1501d93176de3ff74
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2020-06-09 14:28:50 +0300

    Add files via upload

--- C# Basics/02. Simple Operations and Calculations/02. Concatenate Data/Program.cs
+++ C# Basics/02. Simple Operations and Calculations/02. Concatenate Data/Program.cs
@@ -1,1 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace _02.Concatenate_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string firstName = Console.ReadLine();
+            // pishem na konzolata string (ime)
+            string lastName = Console.ReadLine();
+            // pishem na konzolata string (familiq)
+            int age = int.Parse(Console.ReadLine());
+            // pishem na konzolata string (godini)
+            string town = Console.ReadLine();
+            // pishem na konzolata string (grad)
+
+            Console.WriteLine($"You are {firstName} {lastName}, a {age}-years old person from {town}.");
+            // vadim na konzolata text s vuvedenite danni ot konzolata po-rano
+        }
+    }
+}
