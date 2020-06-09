1a05bfb91fe1199b9e1e579a8dd54d584b5434ba
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2020-06-09 14:25:15 +0300

    Create Program.cs

---
+++ C# Basics/01.First Steps/03. Nums 1...10/Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Program
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+			for (int a = 1; a <= 20; a++)
+			{
+				Console.WriteLine(a);
+			}
+		}
+	}
+}
