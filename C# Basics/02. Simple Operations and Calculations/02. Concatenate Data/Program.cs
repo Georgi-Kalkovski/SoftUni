205677a05fdd7141dee824992b87405f2b53ab90
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2020-06-09 14:41:19 +0300

    Update Program.cs

--- C# Basics/02. Simple Operations and Calculations/02. Concatenate Data/Program.cs
+++ C# Basics/02. Simple Operations and Calculations/02. Concatenate Data/Program.cs
@@ -12,16 +12,11 @@ namespace _02.Concatenate_Data
         {
 
             string firstName = Console.ReadLine();
-            // pishem na konzolata string (ime)
             string lastName = Console.ReadLine();
-            // pishem na konzolata string (familiq)
             int age = int.Parse(Console.ReadLine());
-            // pishem na konzolata string (godini)
             string town = Console.ReadLine();
-            // pishem na konzolata string (grad)
 
             Console.WriteLine($"You are {firstName} {lastName}, a {age}-years old person from {town}.");
-            // vadim na konzolata text s vuvedenite danni ot konzolata po-rano
         }
     }
 }
