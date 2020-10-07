d1239fbbffe56d0b08e1636a8931fc57c0f01612
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2020-10-07 15:59:17 +0300

    Adding more code to MVC SUS project

---
+++ 07. C# Web Basics/SUS/SUS.MvcFramework/ViewEngine/SusViewEngine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SUS.MvcFramework.ViewEngine
+{
+    public class SusViewEngine : IViewEngine
+    {
+        public string GetHtml(string templateCode, object viewModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
