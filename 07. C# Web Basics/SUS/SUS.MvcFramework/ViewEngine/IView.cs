899e59c8d1a2bc07838067e1d220ff1108bb9260
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2020-10-08 12:14:56 +0300

    Commit code to SUS

---
+++ 07. C# Web Basics/SUS/SUS.MvcFramework/ViewEngine/IView.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SUS.MvcFramework.ViewEngine
+{
+    public interface IView
+    {
+        string ExecuteTemplate(object viewModel);
+    }
+}
--- 07. C# Web Basics/SUS/SUS.MvcFramework/ViewEngine/SusViewEngine.cs
+++ 07. C# Web Basics/SUS/SUS.MvcFramework/ViewEngine/SusViewEngine.cs
@@ -8,6 +8,51 @@ namespace SUS.MvcFramework.ViewEngine
     {
         public string GetHtml(string templateCode, object viewModel)
         {
+            string csharpCode = GenerateCSharpFromTemplate(templateCode);
+            IView executableObject = GenerateExecutableCode(csharpCode);
+            string html = executableObject.ExecuteTemplate(viewModel);
+            return html;
+        }
+        private string GenerateCSharpFromTemplate(string templateCode)
+        {
+            string methodBody = GetMethodBody(templateCode);
+            string csharpCode = @"
+using System;
+using System.Text;
+using Sytem.Linq;
+using System.Collection.Genereic;
+using SUS.MvcFramework.ViewEngine;
+
+namespace ViewNamespace
+{
+    public class ViewClass : IView
+    {
+        public string ExecuteTemplate(object viewModel)
+        {
+            var html = new StringBuilder();
+
+            " + methodBody + @"
+
+            return html.ToString();
+        }
+
+        private string GetMethodBody(string templateCode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+";
+            return csharpCode;
+        }
+
+        private string GetMethodBody(string templateCode)
+        {
+            return string.Empty;
+        }
+
+        private IView GenerateExecutableCode(string csharpCode)
+        {
             throw new NotImplementedException();
         }
     }
