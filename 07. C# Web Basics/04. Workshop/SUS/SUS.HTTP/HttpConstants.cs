a06d7434862455328c0093292779f2cf06b42d7f
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2020-10-15 08:25:43 +0300

    Changing folder

--- 07. C# Web Basics/04. Workshop/SUS/SUS.HTTP/HttpConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SUS.HTTP
-{
-    public static class HttpConstants
-    {
-        public const int BufferSize = 4096;
-        public const string NewLine = "\r\n";
-        public const string RequestCookieHeader = "Cookie";
-    }
-}
