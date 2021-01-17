c6ee082628005b791f903b8b09bd8a27441069dc
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2021-01-17 13:30:07 +0200

    Update 03SameNumbers.js

--- 09. JS Advanced/01. Syntax, Functions and Statements/Exercise/03SameNumbers.js
+++ 09. JS Advanced/01. Syntax, Functions and Statements/Exercise/03SameNumbers.js
@@ -19,3 +19,24 @@ function SameNumbers(input) {
 
 SameNumbers(2222222);
 SameNumbers(1234);
+
+// function solve(number) {
+//     const string = number.ToString();
+//     let isSame = true;
+//     let sum = 0;
+//     
+//     for (let i = 0; i < string.length; i++) {
+//         let current = Number(string[i]);
+//         let next = string[i + 1];
+//         if (string[i] !== string[i + 1] && next !== undefined){
+//             isSame = false;
+//         }
+//         
+//         sum += current;
+//     }
+//     
+//     return `${isSame}\n${sum}`;
+// }
+// 
+// console.log(solve(2222222));
+// console.log(solve(1234));
