d0f930e1d00ccc6b0e909db6adda147fb8176918
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2021-01-17 14:46:53 +0200

    Update 06CookingByNumbers.js

--- 09. JS Advanced/01. Syntax, Functions and Statements/Exercise/06CookingByNumbers.js
+++ 09. JS Advanced/01. Syntax, Functions and Statements/Exercise/06CookingByNumbers.js
@@ -49,3 +49,23 @@ function CookingByNumbers(input, input1, input2, input3, input4, input5) {
 
 CookingByNumbers('32', 'chop', 'chop', 'chop', 'chop', 'chop');
 CookingByNumbers('9', 'dice', 'spice', 'chop', 'bake', 'fillet');
+
+// function CookingByNumbers(input, input1, input2, input3, input4, input5) {
+// 
+//     let number = Number(input);
+//     const arr = [input1, input2, input3, input4, input5];
+//     
+//     for (let i = 0; i < arr.length; i++) {
+//         switch (arr[i]) {
+//             case 'chop': number /= 2; break;
+//             case 'dice': number = Math.sqrt(number); break;
+//             case 'spice': number += 1; break;
+//             case 'bake': number *= 3; break;
+//             case 'fillet': number = number - (number * 0.20); break;
+//         }
+//         console.log(number)
+//     }
+// }
+// 
+// CookingByNumbers('32', 'chop', 'chop', 'chop', 'chop', 'chop');
+// CookingByNumbers('9', 'dice', 'spice', 'chop', 'bake', 'fillet');
