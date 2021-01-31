a9d63badc993daae7a51886897457cf13b786855
Georgi Kalkovski <Georgi-Kalkovski@users.noreply.github.com>
2021-01-31 22:41:25 +0200

    Adding solver problem 1 from Lab

--- 09. JS Advanced/05. DOM Manipulations and Events/Lab/01. List-Of-Items/app.js
+++ 09. JS Advanced/05. DOM Manipulations and Events/Lab/01. List-Of-Items/app.js
@@ -1,3 +1,9 @@
 function addItem() {
-    console.log('TODO:...');
+    const list = document.querySelector("#items");
+
+    const newLi = document.createElement('li');
+
+    let text = document.querySelector("#newItemText").value;
+    
+    list.appendChild(newLi).textContent = text;
 }
--- 09. JS Advanced/05. DOM Manipulations and Events/Lab/02. Add-Delete/app.js
+++ 09. JS Advanced/05. DOM Manipulations and Events/Lab/02. Add-Delete/app.js
@@ -1,3 +1,7 @@
 function addItem() {
-    //TODO...
+    let text = document.querySelector("#newItemText").value;
+    let list = document.querySelector("#items");
+    let item = document.createElement('a', '[Delete]');
+    item.href = '#';
+    list.appendChild(item).textContent = text;
 }
