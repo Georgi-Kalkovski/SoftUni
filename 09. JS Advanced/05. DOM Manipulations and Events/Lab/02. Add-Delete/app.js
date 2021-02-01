f9deea996570acede30be858266459acd6834b1a
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2021-02-01 09:09:32 +0200

    Adding solved problem 2 from Lab

--- 09. JS Advanced/05. DOM Manipulations and Events/Lab/02. Add-Delete/app.js
+++ 09. JS Advanced/05. DOM Manipulations and Events/Lab/02. Add-Delete/app.js
@@ -1,7 +1,25 @@
 function addItem() {
-    let text = document.querySelector("#newItemText").value;
-    let list = document.querySelector("#items");
-    let item = document.createElement('a', '[Delete]');
-    item.href = '#';
-    list.appendChild(item).textContent = text;
+
+
+    const text = document.querySelector("#newText");
+
+    const li = createElement('li', text.value);
+
+    const a = createElement('a', '[Delete]');
+    a.href = "#";
+    a.addEventListener('click', (event) => {
+        event.target.parentNode.remove();
+        //li.remove();
+    });
+
+    li.appendChild(a); 
+
+    document.querySelector("#items").appendChild(li);
+    
+    function createElement(type, content) {
+        const element = document.createElement(type);
+        element.textContent = content;
+        
+        return element;
+    }
 }
