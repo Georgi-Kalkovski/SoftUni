a1defaba0cc99dd41962160a2e4b6d1bcd4256ba
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2021-02-01 12:46:11 +0200

    Adding problem (under construction)

--- 09. JS Advanced/05. DOM Manipulations and Events/Lab/07. Shopping-Cart/solution.js
+++ 09. JS Advanced/05. DOM Manipulations and Events/Lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,13 @@
 function solve() {
-   //TODO...
+    let products = document.querySelector("body > div").children;
+    let textarea = document.querySelector("body > div > textarea")
+    for (const product of products) {
+        if (product.className == 'product') {
+            let button = product.children[2].children[0];
+            name = product.children[1].children[0].textContent;
+            price = Number(product.children[3].textContent);
+
+            console.log(`Added ${name} for $${price.toFixed(2)} to the cart.` + '\n');
+        }
+    }
 }
