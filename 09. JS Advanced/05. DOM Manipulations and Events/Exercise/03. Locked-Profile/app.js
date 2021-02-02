bf6ebfddf713252327da469d1c3fc29c5b2d697d
Georgi Kalkovski <Georgi-Kalkovski@users.noreply.github.com>
2021-02-02 22:38:32 +0200

    Update app.js

--- 09. JS Advanced/05. DOM Manipulations and Events/Exercise/03. Locked-Profile/app.js
+++ 09. JS Advanced/05. DOM Manipulations and Events/Exercise/03. Locked-Profile/app.js
@@ -1,20 +1,29 @@
 function lockedProfile() {
+
     const profiles = document.getElementById('main').children;
-    for (let i = 0; i < profiles.length; i++) {
-        const profile = profiles[i];
-        const lockedButton = profile.querySelector('input[type=radio]:nth-child(3)');
-        const unlockedButton = profile.querySelector('input[type=radio]:nth-child(5)');
-        const showButton = profile.querySelector('button').addEventListener('click', onClick);
+    forFunction;
+    
+    function forFunction() {
+        for (let i = 0; i < profiles.length; i++) {
+            const profile = profiles[i];
 
-        function onClick() {
-            //const hiddenInfo = e.getElementById('div');
-            const userInfo = profile.querySelector('div>div');
-            const label = document.createElement('label');
-            profile.appendChild(userInfo.children[1])
-            profile.appendChild(userInfo.children[2])
-            profile.appendChild(userInfo.children[3])
-            profile.appendChild(userInfo.children[4])
+            const lockedButton = profile.querySelector('input[type=radio]')
+
+            profile.querySelector('button').addEventListener('click', onClick);
+            let shown = true;
+
+            function onClick() {
+                if (shown == true && !lockedButton.checked) {
+                    profile.querySelector('main>div>div').style.display = 'block';
+                    profile.querySelector('button').childNodes[0].nodeValue = "Hide it";
+                    shown = false;
+                }
+                else if (shown != true && !lockedButton.checked) {
+                    profile.querySelector('main>div>div').style.display = 'none';
+                    profile.querySelector('button').childNodes[0].nodeValue = "Show more";
+                    shown = true;
+                }
+            }
         }
-
     }
 }
