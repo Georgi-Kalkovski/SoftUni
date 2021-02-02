bc92267686f226e00f04bef25749cbaeb893a20c
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2021-02-02 16:08:51 +0200

    Under Construction

--- 09. JS Advanced/05. DOM Manipulations and Events/Exercise/03. Locked-Profile/app.js
+++ 09. JS Advanced/05. DOM Manipulations and Events/Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,20 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const profiles = document.getElementById('main').children;
+    for (let i = 0; i < profiles.length; i++) {
+        const profile = profiles[i];
+        const lockedButton = profile.querySelector('input[type=radio]:nth-child(3)');
+        const unlockedButton = profile.querySelector('input[type=radio]:nth-child(5)');
+        const showButton = profile.querySelector('button').addEventListener('click', onClick);
+
+        function onClick() {
+            //const hiddenInfo = e.getElementById('div');
+            const userInfo = profile.querySelector('div>div');
+            const label = document.createElement('label');
+            profile.appendChild(userInfo.children[1])
+            profile.appendChild(userInfo.children[2])
+            profile.appendChild(userInfo.children[3])
+            profile.appendChild(userInfo.children[4])
+        }
+
+    }
 }
