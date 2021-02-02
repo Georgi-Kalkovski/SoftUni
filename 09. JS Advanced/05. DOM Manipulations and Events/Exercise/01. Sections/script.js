4e1d202b66a9fe5fdf69892cec89bd1482928b6b
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2021-02-02 08:34:01 +0200

    Adding solutions to problem 1-2 from Exercise

--- 09. JS Advanced/05. DOM Manipulations and Events/Exercise/01. Sections/script.js
+++ 09. JS Advanced/05. DOM Manipulations and Events/Exercise/01. Sections/script.js
@@ -1,3 +1,24 @@
 function create(words) {
-   console.log('TODO:...');
+
+    const content = document.getElementById('content');
+
+    for (let i = 0; i < words.length; i++) {
+
+        let div = document.createElement('div');
+        let p = document.createElement('p');
+        p.textContent = words[i];
+        p.style.display = 'none';
+        div.appendChild(p)
+        content.appendChild(div);
+    }
+
+    content.addEventListener('click', onClick)
+
+    function onClick(e) {
+        if (e.target.tagName === 'DIV' || e.target.tagName === 'P') {
+            const p = e.target.children[0] || e.target;
+            const isVisible = p.style.display === 'block';
+            p.style.display = isVisible ? 'none' : 'block';
+        }
+    }
 }
--- 09. JS Advanced/05. DOM Manipulations and Events/Exercise/02. Time-Converter/solution.js
+++ 09. JS Advanced/05. DOM Manipulations and Events/Exercise/02. Time-Converter/solution.js
@@ -1,4 +1,33 @@
 function attachEventsListeners() {
 
-    console.log('TODO:...');
+    let days = document.getElementById('days');
+    let hours = document.getElementById('hours');
+    let minutes = document.getElementById('minutes');
+    let seconds = document.getElementById('seconds');
+
+    let daysBtn = document.getElementById('daysBtn').addEventListener('click', onDaysClick);
+    let hoursBtn = document.getElementById('hoursBtn').addEventListener('click', onHoursClick);
+    let minutesBtn = document.getElementById('minutesBtn').addEventListener('click', onMinutesClick);
+    let secondsBtn = document.getElementById('secondsBtn').addEventListener('click', onSecondsClick);
+
+    function onDaysClick(){
+        hours.value = days.value * 24;
+        minutes.value = days.value * 1440;
+        seconds.value = days.value * 86400;
+    };
+    function onHoursClick(){
+        days.value = hours.value / 24;
+        minutes.value = hours.value * 60;
+        seconds.value = hours.value * 3600;
+     }
+     function onMinutesClick(){ 
+        days.value = minutes.value / 1440;
+        hours.value = minutes.value / 60;
+        seconds.value = minutes.value * 60;
+    }
+    function onSecondsClick(){ 
+        days.value = seconds.value / 86400;
+        hours.value = seconds.value / 3600;
+        minutes.value = seconds.value / 60;
+    }
 }
