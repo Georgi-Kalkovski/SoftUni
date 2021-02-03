e7f713a40e005415f6fe34378c5390ab1f9f9bb7
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2021-02-03 15:59:31 +0200

    Under Construction

--- 09. JS Advanced/05. DOM Manipulations and Events/Exercise/05. Encode-and-Decode-Messages/app.js
+++ 09. JS Advanced/05. DOM Manipulations and Events/Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,9 @@
 function encodeAndDecodeMessages() {
     console.log('TODO...')
+    const textarea = document.querySelector('textarea');
+    document.querySelector('button').addEventListener('click', onClick);
+
+    function onClick(){
+    console.log(textarea)
+    }
 }
