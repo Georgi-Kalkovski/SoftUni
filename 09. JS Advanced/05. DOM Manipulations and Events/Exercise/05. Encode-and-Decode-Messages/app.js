00bcf979c66fbab6e86b47fef0ed8501960284d2
Georgi Kalkovski <Georgi-Kalkovski@users.noreply.github.com>
2021-02-03 23:24:55 +0200

    Adding solution to Problem 5/ 6 Under construction

--- 09. JS Advanced/05. DOM Manipulations and Events/Exercise/05. Encode-and-Decode-Messages/app.js
+++ 09. JS Advanced/05. DOM Manipulations and Events/Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,9 +1,60 @@
-function encodeAndDecodeMessages() {
-    console.log('TODO...')
-    const textarea = document.querySelector('textarea');
-    document.querySelector('button').addEventListener('click', onClick);
+//function encodeAndDecodeMessages() {
+//    const sendMessage = document.querySelector('#main > div:nth-child(1) > textarea');
+//    const receivedMessage = document.querySelector('#main > div:nth-child(2) > textarea');
+//    document.querySelector('#main > div:nth-child(1) > button').addEventListener('click', encodeAndSend);
+//    document.querySelector('#main > div:nth-child(2) > button').addEventListener('click', decodeAndRead);
+//
+//    function encodeAndSend() {
+//        let passingText = '';
+//        sendMessage.value.split('')
+//            .forEach(c => passingText += String.fromCharCode(c.charCodeAt(0) + 1));
+//        receivedMessage.value = passingText;
+//        sendMessage.value = '';
+//        document.querySelector('#main > div:nth-child(2) > button').disabled = false;
+//    }
+//
+//    function decodeAndRead() {
+//        let passingText = '';
+//        receivedMessage.value.split('')
+//            .forEach(c => passingText += String.fromCharCode(c.charCodeAt(0) - 1));
+//        receivedMessage.value = passingText;
+//        document.querySelector('#main > div:nth-child(2) > button').disabled = true;
+//    }
+//}
 
-    function onClick(){
-    console.log(textarea)
-    }
+function encodeAndDecodeMessages() {
+    const textareas = document.querySelectorAll('textarea');
+    const buttons = document.querySelectorAll('button');
+
+    const map = {
+        encode: {
+            text: textareas[0],
+            btn: buttons[0],
+            transform: (char) => String.fromCharCode(char.charCodeAt(0) + 1),
+        },
+        decode: {
+            text: textareas[1],
+            btn: buttons[1],
+            transform: (char) => String.fromCharCode(char.charCodeAt(0) - 1),
+        },
+    };
+
+    document.getElementById('main').addEventListener('click', (e) => {
+        if (e.target.tagName !== 'BUTTON') {
+            return;
+        }
+
+        const type = e.target.textContent
+            .toLowerCase()
+            .trim()
+            .includes('encode') ? 'encode' : 'decode';
+
+        const message = map[type].text.value
+            .split('')
+            .map(map[type].transform)
+            .join('');
+
+        map.encode.text.value = '';
+        map.decode.text.value = message;
+    })
 }
--- 09. JS Advanced/05. DOM Manipulations and Events/Exercise/06. Furniture/solution.js
+++ 09. JS Advanced/05. DOM Manipulations and Events/Exercise/06. Furniture/solution.js
@@ -1,4 +1,14 @@
 function solve() {
+    const inputTextArea = document.querySelector("#exercise > textarea:nth-child(2)")
+    const generateBtn = document.querySelector("#exercise > button:nth-child(3)").addEventListener('click', onGenerateClick);
+    const infoSection = document.querySelector("#exercise > div > div > div > div > table > tbody");
+    const infoOutput = document.querySelector("#exercise > textarea:nth-child(5)")
+    const buyBtn = document.querySelector("#exercise > textarea:nth-child(5)")
 
-  //TODO...
+    function onGenerateClick() {
+        const item = {};
+        item = JSON.parse(inputTextArea.value);
+
+        infoSection.append(item);
+    }
 }
