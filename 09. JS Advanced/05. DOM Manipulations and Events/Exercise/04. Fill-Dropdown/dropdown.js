728f72641b2eee42b242b8f6569919c243a90272
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2021-02-03 12:26:59 +0200

    Adding solution to Problem 4 of the Exercise

--- 09. JS Advanced/05. DOM Manipulations and Events/Exercise/04. Fill-Dropdown/dropdown.js
+++ 09. JS Advanced/05. DOM Manipulations and Events/Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,13 @@
 function addItem() {
-    console.log('TODO:...');
+
+    const newItemText = document.getElementById('newItemText');
+    const newItemValue = document.getElementById('newItemValue');
+
+        const newOption = document.createElement('OPTION');
+        newOption.textContent = newItemText.value;
+        newOption.value = newItemValue.value;
+        const selectItem = document.getElementById('menu');
+        selectItem.appendChild(newOption);
+        newItemText.value = '';
+        newItemValue.value = '';
 }
