555706c4d1539538f3254f00bb358a30d13b1c97
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2021-02-03 07:36:15 +0200

    Added working solution on Problem 3 from Exercise/ Left the old solution as well

--- 09. JS Advanced/05. DOM Manipulations and Events/Exercise/03. Locked-Profile/app.js
+++ 09. JS Advanced/05. DOM Manipulations and Events/Exercise/03. Locked-Profile/app.js
@@ -1,29 +1,42 @@
 function lockedProfile() {
+    [...document.querySelectorAll('#main > div > button')].forEach(button => {
+        button.addEventListener('click', show)
+    })
 
-    const profiles = document.getElementById('main').children;
-    forFunction;
-    
-    function forFunction() {
-        for (let i = 0; i < profiles.length; i++) {
-            const profile = profiles[i];
-
-            const lockedButton = profile.querySelector('input[type=radio]')
-
-            profile.querySelector('button').addEventListener('click', onClick);
-            let shown = true;
-
-            function onClick() {
-                if (shown == true && !lockedButton.checked) {
-                    profile.querySelector('main>div>div').style.display = 'block';
-                    profile.querySelector('button').childNodes[0].nodeValue = "Hide it";
-                    shown = false;
-                }
-                else if (shown != true && !lockedButton.checked) {
-                    profile.querySelector('main>div>div').style.display = 'none';
-                    profile.querySelector('button').childNodes[0].nodeValue = "Show more";
-                    shown = true;
-                }
-            }
+    function show(e) {
+        if (e.target.parentNode.children[4].checked && e.target.textContent === 'Show more') {
+            e.target.parentNode.children[9].style.display = 'block';
+            e.target.textContent = 'Hide it';
+        } else if (e.target.parentNode.children[4].checked) {
+            e.target.parentNode.children[9].style.display = 'none';
+            e.target.textContent = 'Show more';
         }
     }
 }
+
+//function lockedProfile() {
+//
+//    const profiles = document.getElementById('main').children;
+//
+//    for (let i = 0; i < profiles.length; i++) {
+//        const profile = profiles[i];
+//
+//        const lockedButton = profile.querySelector('input[type=radio]')
+//
+//        profile.querySelector('button').addEventListener('click', onClick);
+//        let shown = true;
+//
+//        function onClick() {
+//            if (shown == true && !lockedButton.checked) {
+//                profile.querySelector('main>div>div').style.display = 'block';
+//                profile.querySelector('button').childNodes[0].textContent = "Hide it";
+//                shown = false;
+//            }
+//            else if (shown != true && !lockedButton.checked) {
+//                profile.querySelector('main>div>div').style.display = 'none';
+//                profile.querySelector('button').childNodes[0].textContent = "Show more";
+//                shown = true;
+//            }
+//        }
+//    }
+//}
