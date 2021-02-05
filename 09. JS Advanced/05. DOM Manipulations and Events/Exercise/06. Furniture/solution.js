b69c54dc2dd891b1a6bd232c023c2d3e63ce8b46
Georgi Kalkovski <Georgi-Kalkovski@users.noreply.github.com>
2021-02-05 14:44:51 +0200

    Adding solutions to Problems 6-7 from the Exercise

--- 09. JS Advanced/05. DOM Manipulations and Events/Exercise/06. Furniture/solution.js
+++ 09. JS Advanced/05. DOM Manipulations and Events/Exercise/06. Furniture/solution.js
@@ -1,26 +1,85 @@
 function solve() {
-
-    let textarea = [...document.getElementsByTagName('textarea')];
-    let outputTextarea = textarea[1].value;
-    let table = document.querySelector('table > tbody')
-
-    let buttons = [...document.getElementsByTagName('button')];
-    buttons[0].addEventListener('click', generateBtn);
-    //buttons[1].addEventListener('click', buyBtn)
-
-    function generateBtn() {
-        let jsonParser = JSON.parse(textarea[0].value)
-        for (const item of jsonParser) {
-            let split = item;
-            const obj = {
-                name: split[0],
-                img: split[1],
-                price: Number(split[2]),
-                decFactor: Number(split[3]),
-            };
-
-            console.log(obj);
-
-        }
+    let buttons = [...document.getElementsByTagName("button")];
+  
+    buttons[0].addEventListener('click', generate);
+  
+    buttons[1].addEventListener('click', buy);
+  
+    let textAreas = [...document.getElementsByTagName("textarea")];
+    let table = document.querySelector('table > tbody');
+  
+    function generate() {
+      let inputFurniture = JSON.parse(textAreas[0].value);
+  
+      for (const furniture of inputFurniture) {
+        let trElement = createElement('tr');
+  
+        //Image
+        let tdImg = createElement('td');
+        let img = createElement('img');
+        img.src = furniture.img;
+        tdImg.appendChild(img);
+  
+        //Name
+        let tdName = createElement('td');
+        let name = createElement('p', furniture.name);
+        tdName.appendChild(name);
+  
+        //Price
+        let tdPrice = createElement('td');
+        let price = createElement('p', furniture.price);
+        tdPrice.appendChild(price);
+  
+        //Decoration factor
+        let tdDecorationFactor = createElement('td');
+        let decorationFactor = createElement('p', furniture.decFactor);
+        tdDecorationFactor.appendChild(decorationFactor);
+  
+        let tdCheckbox = createElement('td');
+        let checkbox = createElement('input');
+        checkbox.type = 'checkbox';
+        tdCheckbox.appendChild(checkbox);
+  
+        trElement.appendChild(tdImg);
+        trElement.appendChild(tdName);
+        trElement.appendChild(tdPrice);
+        trElement.appendChild(tdDecorationFactor);
+        trElement.appendChild(tdCheckbox);
+        table.appendChild(trElement);
+      }
     }
-}
+  
+    function buy() {
+      let tableRows = [...document.querySelectorAll('table > tbody > tr')];
+  
+      let boughtProducts = [];
+      let totalSum = 0;
+      let totalFactor = 0;
+  
+      for (const row of tableRows) {
+  
+        if (row.children[4].children[0].checked) {
+          let name = row.children[1].children[0].textContent;
+          let price = Number(row.children[2].children[0].textContent);
+          let decFactor = Number(row.children[3].children[0].textContent);
+  
+          boughtProducts.push(name);
+          totalSum += price;
+          totalFactor += decFactor;
+        }
+      }
+  
+      textAreas[1].value +=
+        `Bought furniture: ${boughtProducts.join(", ")}` +
+        "\n" +
+        `Total price: ${totalSum.toFixed(2)}` +
+        "\n" +
+        `Average decoration factor: ${totalFactor / boughtProducts.length}`;
+    }
+  
+    function createElement(type, content) {
+      const element = document.createElement(type);
+      element.textContent = content;
+      return element;
+    }
+  }
--- 09. JS Advanced/05. DOM Manipulations and Events/Exercise/07. Distance-Converter/distanceConverter.js
+++ 09. JS Advanced/05. DOM Manipulations and Events/Exercise/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,36 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+   
+
+    document.getElementById('convert').addEventListener('click', function () {
+
+        const input = Number(document.getElementById('inputDistance').value);
+        const inputUnits = document.getElementById('inputUnits').value;
+
+        let output = 0;
+        switch (inputUnits) {
+            case 'km': output = input * 1000; break;
+            case 'm': output = input * 1; break;
+            case 'cm': output = input * 0.01; break;
+            case 'mm': output = input * 0.001; break;
+            case 'mi': output = input * 1609.34; break;
+            case 'yrd': output = input * 0.9144; break;
+            case 'ft': output = input * 0.3048; break;
+            case 'in': output = input * 0.0254; break;
+        }
+        
+        const outputUnits = document.getElementById('outputUnits').value;
+
+        switch (outputUnits) {
+            case 'km': output /= 1000; break;
+            case 'm': output /= 1; break;
+            case 'cm': output /= 0.01; break;
+            case 'mm': output /= 0.001; break;
+            case 'mi': output /= 1609.34; break;
+            case 'yrd': output /= 0.9144; break;
+            case 'ft': output /= 0.3048; break;
+            case 'in': output /= 0.0254; break;
+        }
+
+        const outputDistance = document.getElementById('outputDistance').value = output;
+    });
 }
