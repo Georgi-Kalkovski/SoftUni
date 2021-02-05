256f66b17466666abcbb6d6b1742840a4623c991
Georgi Kalkovski <Georgi-Kalkovski@users.noreply.github.com>
2021-02-05 15:28:20 +0200

    Adding solutions to Problem 8-9 from Exercise

--- 09. JS Advanced/05. DOM Manipulations and Events/Exercise/09. JavaScript-Quiz/solution.js
+++ 09. JS Advanced/05. DOM Manipulations and Events/Exercise/09. JavaScript-Quiz/solution.js
@@ -1,3 +1,43 @@
 function solve() {
-  //TODO...
-}
+    let correctAnswers = [
+      "onclick",
+      "JSON.stringify()",
+      "A programming API for HTML and XML documents",
+    ];
+  
+    [...document.getElementsByTagName('p')].forEach(b => {
+      b.addEventListener('click', getNextSection)
+    });
+  
+    let sections = [...document.getElementsByTagName("section")];
+  
+    let counterCorrectAnswers = 0;
+    let counterSections = 0;
+  
+    function getNextSection(event) {
+      let currentAnswer = event.target.textContent;
+  
+      if (correctAnswers.includes(currentAnswer)) {
+        counterCorrectAnswers++;
+      }
+  
+      sections[counterSections].style.display = 'none';
+      counterSections++;
+  
+      if (counterSections === sections.length) {
+        let resultParent = document.getElementById("results");
+        resultParent.style.display = "block";
+  
+        let resultInput = resultParent.children[0].children[0];
+  
+        if (counterCorrectAnswers === 3) {
+          resultInput.textContent = "You are recognized as top JavaScript fan!";
+        } else {
+          resultInput.textContent = `You have ${counterCorrectAnswers} right answers`;
+        }
+  
+      } else {
+        sections[counterSections].style.display = "block";
+      }
+    }
+  }
