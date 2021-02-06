6a58ad2b0df010fd495a0ef8859353ce96d687e0
Georgi Kalkovski <Georgi-Kalkovski@users.noreply.github.com>
2021-02-06 19:37:05 +0200

    Adding solution to Problem 7

--- 09. JS Advanced/06. Advanced Functions/Exercise/07. Simple Calculator/app.js
+++ 09. JS Advanced/06. Advanced Functions/Exercise/07. Simple Calculator/app.js
@@ -1,7 +1,24 @@
 function calculator() {
-    // TODO:
+    let firstElement;
+    let secondElement;
+    let resultElement;
+    return {
+        init: (selector1, selector2, resultSelector) => {
+            firstElement = document.querySelector(selector1);
+            secondElement = document.querySelector(selector2);
+            resultElement = document.querySelector(resultSelector);
+        },
+        add: () => {
+            resultElement.value = Number(firstElement.value) + Number(secondElement.value);
+        },
+        subtract: () => {
+            resultElement.value = Number(firstElement.value) - Number(secondElement.value);
+        }
+    }
 }
 
-
-
-
+let obj = calculator()
+console.log(obj);
+obj.init('#num1', '#num2', '#result');
+let add = obj.add;
+let subtract = obj.subtract;
