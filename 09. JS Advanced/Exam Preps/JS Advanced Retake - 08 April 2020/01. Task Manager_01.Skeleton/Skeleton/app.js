17057000e9dfca72c92045172ccca6b8ab7330ed
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2021-02-12 08:45:57 +0200

    Fixing code

--- 09. JS Advanced/Exam Preps/JS Advanced Retake - 08 April 2020/01. Task Manager_01.Skeleton/Skeleton/app.js
+++ 09. JS Advanced/Exam Preps/JS Advanced Retake - 08 April 2020/01. Task Manager_01.Skeleton/Skeleton/app.js
@@ -1,13 +1,20 @@
 function solve() {
+
     const task = document.getElementById('task');
     const description = document.getElementById('description');
     const date = document.getElementById('date');
-    const open = document.querySelector('section:nth-child(2) > div:nth-child(2)');
-    const inProgress = document.getElementById('in-progress');
-    document.getElementById('add')
-        .addEventListener('click', onClick);
 
-    function onClick(e) {
+    const add = document.getElementById('add');
+    const open = document.querySelector('section:nth-child(2)').children[1];
+    const inProgress = document.querySelector('section:nth-child(3)').children[1];
+    const completed = document.querySelector('section:nth-child(4)').children[1];
+
+
+    add.addEventListener('click', onAddTask);
+    open.addEventListener('click', onOpen);
+    inProgress.addEventListener('click', onInProgress);
+
+    function onAddTask(e) {
         e.preventDefault();
         if (task.value === '' ||
             description.value === '' ||
@@ -15,42 +22,68 @@ function solve() {
             return;
         }
         const article = document.createElement('article');
-        const h3 = document.createElement('h3')
+
+        const h3 = document.createElement('h3');
         h3.textContent = task.value;
-        const p1 = document.createElement('p')
+
+        const p1 = document.createElement('p');
         p1.textContent = description.value;
-        const p2 = document.createElement('p')
+
+        const p2 = document.createElement('p');
         p2.textContent = date.value;
-        const div = document.createElement('div')
+
+        const div = document.createElement('div');
         div.className = 'flex';
-        const button1 = document.createElement('button')
+
+        const button1 = document.createElement('button');
         button1.className = 'green';
         button1.textContent = 'Start';
-        const button2 = document.createElement('button')
+
+        const button2 = document.createElement('button');
         button2.className = 'red';
         button2.textContent = 'Delete';
+
         div.appendChild(button1);
         div.appendChild(button2);
         article.appendChild(h3);
         article.appendChild(p1);
         article.appendChild(p2);
         article.appendChild(div);
+
         open.appendChild(article);
     }
 
-    open.addEventListener('click', function (e) {
+    function onOpen(e) {
         if (e.target.className === 'red') {
             e.target.parentNode.parentNode.remove();
         } else if (e.target.className === 'green') {
             const button1 = e.target.parentNode.children[0];
             button1.className = 'red';
             button1.textContent = 'Delete';
+
             const button2 = e.target.parentNode.children[1];
             button2.className = 'orange';
             button2.textContent = 'Finish';
+
             const article = e.target.parentNode.parentNode;
+
             e.target.parentNode.parentNode.remove();
+
             inProgress.appendChild(article);
         }
-    });
-};
+    };
+
+    function onInProgress(e) {
+        if (e.target.className === 'red') {
+            e.target.parentNode.parentNode.remove();
+        }
+        else if (e.target.className === 'orange') {
+            const article = e.target.parentNode.parentNode;
+
+            e.target.parentNode.parentNode.remove();
+            article.children[3].remove();
+
+            completed.appendChild(article);
+        }
+    };
+}
---
+++ 09. JS Advanced/Exam Preps/JS Advanced Retake - 08 April 2020/02 .Repository_02.Resources/Resources/tests.js
@@ -0,0 +1,10 @@
+let { Repository } = require("./solution.js");
+
+describe("Tests …", function () {
+    describe("TODO …", function () {
+        it("TODO …", function () {
+            // TODO: …
+        });
+    });
+    // TODO: …
+});
