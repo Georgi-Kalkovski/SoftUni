3cfcb4df520ee06e8b10906701e776e98f3471cf
Georgi Kalkovski <Georgi-Kalkovski@users.noreply.github.com>
2021-02-14 13:41:39 +0200

    Fixing folders

---
+++ 09. JS Advanced/Exam Preps/JS Advanced Retake - 08 April 2020/03.Bank.js
@@ -0,0 +1,81 @@
+class Bank {
+    constructor(bankName) {
+        this.bankName = bankName;
+        this.allCustomers = [];
+        this.transactions = [];
+    }
+
+    personCheck(personalId){
+        let personFound = this.allCustomers.find(e => e.personalId == personalId);
+        if (!personFound) {
+            throw new Error(`We have no customer with this ID!`);
+        }
+    }
+
+    newCustomer(customer) {
+        let newCustomer = {
+            firstName: customer.firstName,
+            lastName: customer.lastName,
+            personalId: customer.personalId
+        }
+        let customerCheck = this.allCustomers.find(e => e.personalId == newCustomer.personalId);
+        if (customerCheck) {
+            throw new Error(`${newCustomer.firstName} ${newCustomer.lastName} is already our customer!`)
+        }
+        this.allCustomers.push(newCustomer)
+        return newCustomer;
+    }
+    depositMoney(personalId, amount) {
+        personCheck(personalId);
+        if (!personFound.totalMoney) {
+            personFound.totalMoney = Number(0);
+        }
+        personFound.totalMoney += Number(amount);
+        this.transactions.push([personFound.firstName, personFound.lastName, 'made depostit of', Number(amount)]);
+        return `${personFound.totalMoney}$`;
+    }
+    withdrawMoney(personalId, amount) {
+        personCheck(personalId);
+        if (personFound.totalMoney < amount) {
+            throw new Error(`${personFound.firstName} ${personFound.lastName} does not have enough money to withdraw that amount!`);
+        }
+        this.transactions.push([personFound.firstName, personFound.lastName, 'withdrew', Number(amount)]);
+        personFound.totalMoney -= amount;
+        return `${personFound.totalMoney}$`;
+    }
+    customerInfo(personalId) {
+        personCheck(personalId);
+        let result = `Bank name: ${this.bankName}\n` +
+            `Customer name: ${personFound.firstName} ${personFound.lastName}\n` +
+            `Customer ID: ${personFound.personalId}\n` +
+            `Total Money: ${personFound.totalMoney}$\n` +
+            `Transactions:\n`
+        let counter = 0;
+        for (const transaction of this.transactions) {
+            if (transaction[0] == personFound.firstName && transaction[1] == personFound.lastName) {
+                counter++;
+            }
+        }
+        for (const transaction of this.transactions.reverse()) {
+            if (transaction[0] == personFound.firstName && transaction[1] == personFound.lastName) {
+                result += `${counter}. ${transaction[0]} ${transaction[1]} ${transaction[2]} ${transaction[3]}$!\n`;
+                counter--;
+            }
+        }
+        return result;
+    }
+}
+
+let bank = new Bank('SoftUni Bank');
+
+console.log(bank.newCustomer({ firstName: 'Svetlin', lastName: 'Nakov', personalId: 6233267 }));
+console.log(bank.newCustomer({ firstName: 'Svetlin', lastName: 'Nakov', personalId: 6233267 }));
+console.log(bank.newCustomer({ firstName: 'Mihaela', lastName: 'Mileva', personalId: 4151596 }));
+
+bank.depositMoney(6233267, 250);
+console.log(bank.depositMoney(6233267, 250));
+bank.depositMoney(4151596, 555);
+
+console.log(bank.withdrawMoney(6233267, 125));
+
+console.log(bank.customerInfo(6233267));
