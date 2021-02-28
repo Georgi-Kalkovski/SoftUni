994efff1156618b66ebba913f7b258e06486a113
Georgi Kalkovski <Georgi-Kalkovski@users.noreply.github.com>
2021-02-28 13:12:18 +0200

    Adding JS Apps Workshops

---
+++ 10. JS Applications/JS Apps Workshop/lesson-06/finished/src/navigation.js
@@ -0,0 +1,87 @@
+import { render } from './dom.js';
+
+
+export function createNav(main, navbar) {
+    const views = {};
+    const links = {};
+    const forms = {};
+
+    setupNavigation();
+    setupForms();
+
+    const navigator = {
+        registerView,
+        goTo,
+        setUserNav,
+        registerForm
+    };
+
+    return navigator;
+
+    function setupNavigation() {
+        navbar.addEventListener('click', (ev) => {
+            if (ev.target.tagName == 'A') {
+                const handlerName = links[ev.target.id];
+                if (handlerName) {
+                    ev.preventDefault();
+                    goTo(handlerName);
+                }
+            }
+        });
+    }
+
+    async function goTo(name, ...params) {
+        const result = await views[name](...params);
+        render(result, main);
+    }
+
+    function registerView(name, setup, navId) {
+        const execute = setup(navigator);
+
+        views[name] = (...params) => {
+            [...navbar.querySelectorAll('a')].forEach(a => a.classList.remove('active'));
+            if (navId) {
+                navbar.querySelector('#' + navId).classList.add('active');
+            }
+            return execute(...params);
+        };
+        if (navId) {
+            links[navId] = name;
+        }
+    }
+
+    function setUserNav() {
+        if (sessionStorage.getItem('userToken') != null) {
+            document.getElementById('user').style.display = 'inline-block';
+            document.getElementById('guest').style.display = 'none';
+        } else {
+            document.getElementById('user').style.display = 'none';
+            document.getElementById('guest').style.display = 'inline-block';
+        }
+    }
+
+    function setupForms() {
+        document.body.addEventListener('submit', createFormData);
+        document.body.addEventListener('formdata', onFormData);
+    }
+
+    function registerForm(name, handler) {
+        forms[name] = handler;
+    }
+
+    function createFormData(ev) {
+        if (forms.hasOwnProperty(ev.target.id)) {
+            ev.preventDefault();
+            new FormData(ev.target);
+        }
+    }
+
+    function onFormData(ev) {
+        if (forms.hasOwnProperty(ev.target.id)) {
+            const body = [...ev.formData.entries()].reduce((p, [k, v]) => Object.assign(p, { [k]: v }), {});
+            const handler = forms[ev.target.id];
+            handler(body);
+        }
+    }
+}
+
