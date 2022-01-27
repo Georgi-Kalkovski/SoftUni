f4b4a0008a2d5ac9b80ad6102ea0ee14b8233305
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2022-01-27 12:30:24 +0200

    Adding workshop

---
+++ 13. JS Back-End/03. ExpressJS and Templating/Workshop/03. Cubicle-Workshop-Part-1-Resources/config/express.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+
+module.exports = (app) => {
+    
+    //TODO: Setup the view engine
+
+    //TODO: Setup the body parser
+
+    //TODO: Setup the static files
+
+};
